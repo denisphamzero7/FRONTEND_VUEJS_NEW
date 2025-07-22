a09ee65c5e3b11bd5995907ac91232b88defdcf6
https://github.com/denisphamzero7/DAN <denisphamzero7>
2025-07-22 19:01:55 +0700

    vue 1-2

---
+++ part1/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+<h1>the first app</h1>
+
+<button @click="add" type="button">add</button>
+<span>{{ count }}</span>
+<button @click="remove" type="button">remove</button>
+<p v-if="count>= 10">ohh cool off due</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const count = ref(0)
+const add = ()=>{
+  count.value++
+}
+const remove =()=>{
+  if(count.value >0){
+    count.value--
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
