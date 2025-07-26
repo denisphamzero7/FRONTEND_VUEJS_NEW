ae0d0149ac8da048f97337413aa9454d32d77b83
https://github.com/denisphamzero7/DAN <denisphamzero7>
2025-07-26 18:55:16 +0700

    update code

--- vue-settings/src/App.vue
+++ vue-settings/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { Transition, computed, ref } from 'vue';
 import type { Tab, TabKey } from './types';
 import TabLink from '@/components/TabLink.vue';
 
@@ -20,14 +20,16 @@ const currentTab = ref<TabKey>('General');
 
 <template>
   <main class="max-w-2xl max-auto px-4">
-    <nav class="font-medium text-center text-gray-500 border-gray-200 mb-4">
+    <nav class="hidden sm:block font-medium text-center text-gray-500 border-gray-200 mb-4">
       <ul class="flex flex-wrap -mb-px">
         <li v-for="tab in tabs" :key="tab.key">
           <TabLink :tab="tab" :currentTab="currentTab" @click="currentTab = tab.key as TabKey"/>
         </li>
       </ul>
+
     </nav>
-  <FadeTransition ><component :is="currentTabComponent "/></FadeTransition>
+  <FadeTransition><component :is="currentTabComponent "/></FadeTransition>
+
   </main>
 </template>
 <style scoped>
--- vue-settings/src/components/GeneralSettings.vue
+++ vue-settings/src/components/GeneralSettings.vue
@@ -1,5 +1,64 @@
 <template>
-  <div>General</div>
+  <div>
+    <div>General</div>
+
+  <form action="" class="space-y-4 mx-auto">
+
+    <div>
+
+      <label for="">
+        Username
+      </label>
+      <input type="text">
+    </div>
+    <div>
+
+<label for="">
+Email
+</label>
+<input type="text">
+</div>
+<div>
+
+<label for="">
+  About me
+</label>
+<textarea name="" id=""></textarea>
+</div>
+<div>
+
+<label for="">
+  Gender
+</label>
+<label for=""><input type="radio" value="male"></input>
+  <span>Male</span>
+</label>
+<label for=""><input type="radio" value="male"></input>
+  <span>Female</span>
+</label>
+<label >
+  <input type="radio" value="other">
+  <span>other</span>
+</label>
+</div>
+
+<div>
+
+<label for="">
+  country
+</label>
+<select >
+  <option>Usa</option>
+  <option>vietnam
+  </option>
+  <option>usa</option>
+</select>
+</div>
+<button type="submit" class="btn-primary">submit</button>
+  </form>
+
+
+  </div>
 </template>
 
 <script setup lang="ts">
